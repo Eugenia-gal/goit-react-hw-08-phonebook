3789329d3d042f05f75984acfcfa8fd4b493bca1
Eugenia-gal <galevbor12@gmail.com>
2021-09-01 18:41:01 +0300

    add routing rules

---
+++ src/Components/Routes/PrivateRoute.jsx
@@ -0,0 +1,17 @@
+import { useSelector } from 'react-redux';
+import { Route, Redirect } from 'react-router-dom';
+import authSelectors from 'redux/Authorization/auth-selectors';
+
+export default function PrivateRoute({
+  children,
+  redirectTo = '/login',
+  ...routeProps
+}) {
+  const isLoggedIn = useSelector(authSelectors.getIsLoggedIn);
+
+  return (
+    <Route {...routeProps}>
+      {isLoggedIn ? children : <Redirect to={redirectTo} />}
+    </Route>
+  );
+}
